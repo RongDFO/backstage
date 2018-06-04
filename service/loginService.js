e11d15fe801ad537130fd86c7c1d1aac31433b4a
RongDFO <1924106306@qq.com>
2018-06-04 18:15:48 +0800

    修改登陆bug

--- service/loginService.js
+++ service/loginService.js
@@ -5,22 +5,29 @@ const config = require('../config/config');
 module.exports = {
     async loginService(params){
         const loginres  = await loginDao.loginDao(params);
-        const newpass = util.getSaltPassword(params.password,loginres[0].salt);
-        console.log(newpass)
-        console.log(loginres[0].password)
-        if(loginres.length>0&&newpass==loginres[0].password){
-            const token = jwt.sign({
-                username:params.username,
-                password:params.password,
-            },'app.get(superSecret)',{
-                expiresIn : config.tokenExpiration// 授权时效24小时
-              });
-            const code = {
-                code : 1,
-                msg : '登陆成功',
-                token:token
+      
+        if(loginres.length>0){
+            const newpass = util.getSaltPassword(params.password,loginres[0].salt);
+            if(newpass==loginres[0].password){
+                const token = jwt.sign({
+                    username:params.username,
+                    password:params.password,
+                },'app.get(superSecret)',{
+                    expiresIn : config.tokenExpiration// 授权时效24小时
+                  });
+                const code = {
+                    code : 1,
+                    msg : '登陆成功',
+                    token:token
+                }
+                return code;
+            }else{
+                const code = {
+                    code : 0,
+                    msg : '账号或密码错误',
+                }
+                return code;
             }
-            return code;
         }else{
             const code = {
                 code : 0,
